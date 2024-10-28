bf6e42e42e63f728a5ef7de6da389e73d5bb4782
Sam MacGinty <34136596+sammac22@users.noreply.github.com>
2024-10-28 13:44:45 -0400

    Initial Commit

---
+++ IntelligenceAssistant/IntelligenceAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligenceAssistantApp.swift
+//  IntelligenceAssistant
+//
+//  Created by Sam MacGinty on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct IntelligenceAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
